f47b785e7195c1bf27f4bad3f35c8fe80df35f32
Adrian Cisneros <nicolocisneros@gmail.com>
2024-10-01 22:39:21 -0500

    feat: Creacion del proyecto, Dependencias generadas

---
+++ src/main/java/com/example/caso_prestamos/CasoPrestamosApplication.java
@@ -0,0 +1,13 @@
+package com.example.caso_prestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoPrestamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoPrestamosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caso_prestamos/CasoPrestamosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caso_prestamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoPrestamosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
