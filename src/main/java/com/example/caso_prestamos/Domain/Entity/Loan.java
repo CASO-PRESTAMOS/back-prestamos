e0194c9b8cc2c6a9261a8a48c9b9ee212580e5e2
SebastianGilian <mgiliana1@upao.edu.pe>
2024-10-05 15:53:17 -0500

    merge de loan y auth

--- src/main/java/com/example/caso_prestamos/Domain/Entity/Loan.java
+++ src/main/java/com/example/caso_prestamos/Domain/Entity/Loan.java
@@ -3,19 +3,21 @@ package com.example.caso_prestamos.Domain.Entity;
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Table(name = "loan")
+@NoArgsConstructor
 public class Loan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long loanID;
 
-    @Column(name="client_id", nullable = false)
-    private Long clientId;
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false, foreignKey = @ForeignKey(name = "FK_user_loan"))
+    private User userID;
 
     @Column(name = "amount", nullable = false)
     private double amount;
@@ -33,6 +35,6 @@ public class Loan {
     @Column(name = "duration", nullable = false)
     private Integer duration;
 
-    @Column(name = "interes_rate", nullable = false)
+    @Column(name = "interest_rate", nullable = false)
     private Double interestRate;
 }
--- src/main/java/com/example/caso_prestamos/Web/Controller/LoanController.java
+++ src/main/java/com/example/caso_prestamos/Web/Controller/LoanController.java
@@ -3,20 +3,24 @@ package com.example.caso_prestamos.Web.Controller;
 
 import com.example.caso_prestamos.Domain.Entity.Loan;
 import com.example.caso_prestamos.Service.LoanService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("admin/loan")
+@RequestMapping("/admin/loan")
+@PreAuthorize("hasRole('ADMIN')")
 public class LoanController {
     private final LoanService loanService;
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<List<Loan>> getAll(){
         List<Loan> loans = loanService.getAll();
         return new ResponseEntity<List<Loan>>(loans, HttpStatus.OK);
@@ -28,13 +32,13 @@ public class LoanController {
         return new ResponseEntity<Loan>(loan, HttpStatus.OK);
     }
 
-    @PostMapping
-    public ResponseEntity<Loan> create(@RequestBody Loan loan){
+    @PostMapping("/create")
+    public ResponseEntity<Loan> create(@RequestBody @Valid Loan loan){
         Loan newLoan = loanService.create(loan);
-        return new ResponseEntity<Loan>(newLoan,HttpStatus.CREATED);
+        return new ResponseEntity<>(newLoan,HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("update/{id}")
     public ResponseEntity<Loan> update(@PathVariable("id") Long id,
                                        @RequestBody Loan loan){
         Loan updatedLoan = loanService.update(id, loan);
