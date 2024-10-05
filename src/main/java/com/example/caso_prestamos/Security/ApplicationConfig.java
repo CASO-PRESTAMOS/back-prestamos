395bfa248163f8d3acf5f053a2b5437d2b30e633
Adrian Cisneros <nicolocisneros@gmail.com>
2024-10-05 13:57:56 -0500

    feat: Corrección en los imports de AppConfig

--- src/main/java/com/example/caso_prestamos/Security/ApplicationConfig.java
+++ src/main/java/com/example/caso_prestamos/Security/ApplicationConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
